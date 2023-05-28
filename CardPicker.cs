dc671638aa0d15490d145c6a7fbaa795e57185fa
Karina Khoninova <k.honinova@yandex.ru>
2023-05-28 19:55:26 +0300

    Добавьте файлы проекта.

---
+++ CardPicker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            // получить случайное число от 1 до 4
+            int value = random.Next(1, 5);
+            // если это 1, вернуть строку Spades
+            if (value == 1) return "Spades";
+            // если это 2, вернуть строку Hearts
+            if (value == 2) return "Hearts";
+            // если это 3, вернуть строку Clubs
+            if (value == 3) return "Clubs";
+            // если выполнение продолжается, вернуть строку Diamonds
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                // этот блок выполняется в том случае, если строка МОЖЕТ БЫТЬ преобразована в int
+                // значение, сохраняемое в новой переменной, называется numberOfCards
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                // этот блок выполняется, если строка НЕ МОЖЕТ БЫТЬ преобразована в int
+                Console.WriteLine("Число недействительно.");
+            }
+            
+
+        }
+    }
+}
